a8dc9955d2828b790beda1cb29271e46f25ba96a
Ilya Sadigurski <isadigurski@gmail.com>
2020-02-14 11:04:45 -0500

    Beta Version

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function ask(questionText) {
 // remember the StateMachine lecture
 // https://bootcamp.burlingtoncodeacademy.com/lessons/cs/state-machines
 let states = {
-  'roomOne': { canChangeTo: [ 'roomTwo' ] },
-  'roomTwo': { canChangeTo: [ 'roomThree' ] },
-  'roomThree': { canChangeTo: [ 'roomOne' ] }
+  'roomOne': { canChangeTo: ['roomTwo'] },
+  'roomTwo': { canChangeTo: ['roomThree'] },
+  'roomThree': { canChangeTo: ['roomOne'] }
 };
 
 let currentState = "green";
@@ -25,15 +25,53 @@ function enterState(newState) {
     throw 'Invalid state transition attempted - from ' + currentState + ' to ' + newState;
   }
 }
+//Room Template.................................................
+class Room {
+  constructor(desc, inv, locked) {
+    this.desc = desc
+    this.inv = inv
+    this.locked = locked
+  }
+}
+
+let insideOne = new Room('', [], true)
+let insideTwo = new Room('', [], true)
+let insideThree = new Room('', [], true)
+let insideFour = new Room('', [], true)
+let insideFive = new Room('', [], true)
+//..............................................................
+let yesResponse = ['Y', 'Yes', 'YES', 'yes', 'y']
+let noResponse = ['N', 'No', 'NO', 'no', 'n']
+//let theResponse = ""
+//..............................................................
 
 start();
 
 async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
-  let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
-  process.exit();
-}
+//player1.name = null;
+//player1.name = userName;
+  let response = await ask("\nI want to play a game. It's similar to the game you play.  Would you like to continue Y or N?\n\n>_")
+
+  if (noResponse.includes(response)) {
+    console.log("Goodbyes")
+    process.exit()
+    //playerName = null;
+    //plyerName = userName;
+  } else if (yesResponse.includes(response)) {
+    let userName = await ask("\nWhat is your name player?\n\n>_")
+
+    //let response = await ask("\nMost people are so ungrateful to be alive but not you" + playerName)
+ } else {
+   console.log("Peace Out")
+   process.exit()
+ }
+ //.............................................................
+let getResponseMainSt = thePrompt => {
+  let theResponse = "";
+
+ if (thePrompt === "read sign\n\n") {
+   theResponse = "The sign says 'Welcome to Burlington Code Academy! Come on up to the third floor. If the door is locked, use the code12345'";
+ } else if (thePrompt === "take sign") {
+   theResponse = "The door is locked. There is a keypad on the door handle. \n"
+ } return theResponse;
+}};
