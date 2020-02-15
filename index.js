b64708ccdd3b94783a18966e47ba81795cd0e1c7
ZenArcade17 <59713429+ZenArcade17@users.noreply.github.com>
2020-02-15 14:24:58 -0500

    whatever

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ async function start() {
   console.log("\nMost people are so ungrateful to be alive but not you." +
     "\nYou are standing outside 182 Main Street between Church and South Winooski." +
     "\nThere is a door here. A keypad sits on the handle.  On the door is a handwritten sign.\n")
+  playerObj.location = 'Outside Main St'
   while (response !== 'exit') {
     response = await ask('>_')
     if (noResponse.includes(response)) {
@@ -86,37 +87,51 @@ async function start() {
     } else if (response.toLowerCase() === "grab items") {
       response = console.log("\nYou grab the items and add them to your inventory.\n")
     } else if (response.toLowerCase() === "go up stairs") {
+      playerObj.location = 'In the Hallway'
       response = console.log("\nYou walk up the stairs and enter a hallway with five doors numbered 1 through 5\n")
+      console.log(playerObj) //check
       
       //..............................................................King's Landing
     } else if (response.toLowerCase() === "enter door 1") {
+      playerObj.location = 'Kings Landing'
       response = console.log(`\nYou have entered the "King's Landing". A strange individial is sitting at a desk mumbling about a missing Seven Days\n`)
+      console.log(playerObj) //check
     } else if (response.toLowerCase() === "give seven days") {
       response = console.log('\nThe strange man looks into your eyes, flips you a coin and says "Keep the change you filthy animal"\n')
     } else if (response.toLowerCase() === "exit room") {
+      playerObj.location = 'Hallway'
       response = console.log("\nYour're back in the hallway stairing at a 3D photo\n")
+      console.log(playerObj) //check
       //...............................................................Cherry Garcia
     } else if (response.toLowerCase() === "enter door 2") {
+      playerObj.location = 'cherry garcia room'
       response = console.log(`\nThere is a freezer in the middle of the room, do you want to "put the ice cream in freezer"\n`)
+      console.log(playerObj) //check
     } else if (response.toLowerCase() === "put the ice cream in the freezer") {
       response = console.log(`\nYour Strawberry Cheese Cake ice cream will be safe in here, go to room 4 to get a spoon\n`)
-    } else if (response.toLowerCase() === "exit room") {
+    } else if (response.toLowerCase() === "exit room" && playerObj.location == 'cherry garcia room') {
+      playerObj.location = 'Hallway'
       response = console.log("\nYou're back in the hallway staring at the 3D photo again. A kid tugs your pants and says its a schooner." +
         `You reply it's not a schooner...It's a Sailboat.  The little boy replies with "A schooner IS a sailboat stupid head!"\n`)
+        console.log(playerObj) //check  
       //.....................................................................Jumanji
     } else if (response.toLowerCase() === "enter door 3") {
       response = console.log(`\nDoor is locked. Where are the keys?\n`)
+      console.log(playerObj) //check
     } else if (response.toLowerCase() === "use keys") {
       response = console.log(`\nDoor unlocks, enter room\n`)
     } else if (response.toLowerCase() === "enter room") {
       response = console.log(`\nA lion roars and you immediately close the door\n`)
     } else if (response.toLowerCase() === "exit room") {
+      playerObj.location = 'In the Hallway'
       response = console.log("\nYou're in the hallway now\n")
+      console.log(playerObj) //check
       //................................................................Office Space
     } else if (response.toLowerCase() === "enter door 4") {
       response = console.log(`\nWelcome to the kitchen.\nLet me give you a tour.` + 
       `\nHere we have a microwave, dishwasher, fridge, and some cupboards.` +
       `\nFeel free to USE anything, but don't forget to clean up after yourself\n`)
+      console.log(playerObj) //check
     } else if (response.toLowerCase() === "use microwave") {
       response = console.log(`\nClose the door it smells like someone nuked some fish in there.  The smell covers the whole room.\n`)
     } else if (response.toLowerCase() === "use dishwasher") {
@@ -130,14 +145,19 @@ async function start() {
     } else if (response.toLowerCase() === "trade knife") {
       response = console.log(`\nYou may now "TAKE" the GOLDEN SPOON\n`)
     } else if (response.toLowerCase() === "exit room") {
+      playerObj.location = 'In the Hallway'
       response = console.log(`\nYou're in the hallway now\n`)
+      console.log(playerObj) //check
       //.................................................................Escape Room
     } else if (response.toLowerCase() === "enter door 5") {
       response = console.log("\nIt's a room inside a room inside a room...if you want to exit I'll give you a hint.\n")
+      console.log(playerObj) //check
     } else if (response.toLowerCase() === "hint") {
       response = console.log(`\nReverse Text The Name of The Room You're In\n`)
     } else if (response.toLowerCase() === "mooR epacsE") {
+      
       response = console.log(`\nYou're in the hallway now\n`)
+      console.log(playerObj) //check
       //........................................................................Exit   
     } else if (response.toLowerCase() === "end game") {
       console.log("\nPeace Out\n")
